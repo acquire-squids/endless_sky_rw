b82f4c91848683ecd8b34e59ab8a115522e745ca
acquire-squids <62000632+acquire-squids@users.noreply.github.com>
2025-11-17 04:12:30 -0500

    Fix macro `tree_from_tokens`
    It had some code from the old version that I copied because I didn't want to figure out the macro again.
    Should actually work now.

--- src/macros.rs
+++ src/macros.rs
@@ -59,6 +59,8 @@ macro_rules! tree_from_tokens {
         {
             let mut data: &mut $crate::Data = $data;
 
+            let source_index: $crate::SourceIndex = $source_index;
+
             let parent: $crate::NodeIndex = $node;
 
             $(
@@ -85,15 +87,17 @@ macro_rules! tree_from_tokens {
 
             let source_index: $crate::SourceIndex = $source_index;
 
-            let node = {
+            if data.get_source(source_index).is_none() {
+                data.error_node()
+            } else {
                 let node = data.insert_node(Node::Some { tokens: vec![], });
 
-                let span = data.push_source(source_index, $token);
+                let span = data.push_source(source_index, $token).unwrap();
                 data.push_token(node, Token::new(TokenKind::Symbol, Span::new(span.0, span.1)));
 
                 $(
-                    let _ = write!(buffer, "{}", $tokens);
-                    data.push_token(node, Token::new(TokenKind::Symbol, Span::new(buffer_len, buffer.len())));
+                    let span = data.push_source(source_index, $tokens).unwrap();
+                    data.push_token(node, Token::new(TokenKind::Symbol, Span::new(span.0, span.1)));
                 )*
 
                 $(
@@ -101,9 +105,7 @@ macro_rules! tree_from_tokens {
                 )?
 
                 node
-            };
-
-            (buffer, node)
+            }
         }
     };
 }
